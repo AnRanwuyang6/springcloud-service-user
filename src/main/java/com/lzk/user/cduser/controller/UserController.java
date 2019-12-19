a042a096ae004f4304a95c6517d9f4c5daa1b82e
lizk <zong65432@163.com>
2019-12-19 17:11:43 +0800

    添加到springcloud bus

--- src/main/java/com/lzk/user/cduser/controller/UserController.java
+++ src/main/java/com/lzk/user/cduser/controller/UserController.java
@@ -3,16 +3,22 @@ package com.lzk.user.cduser.controller;
 import com.lzk.user.cduser.dao.SysUserMapper;
 import com.lzk.user.cduser.entity.SysUser;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Date;
 
 @RestController
+@RefreshScope
 @RequestMapping("/service/user")
 public class UserController {
     @Autowired
     private SysUserMapper sysUserMapper;
+
+    @Value("${name}")
+    private String name;
 
     @RequestMapping("/findAll")
     public String findAll(){
@@ -23,5 +29,10 @@ public class UserController {
         sysUserMapper.insert(sysUser);
         System.out.println("user服务");
         return "user服务";
+    }
+
+    @RequestMapping("/test")
+    public String test(){
+        return name;
     }
 }
